0ef6592a330d4e3852bece058776626a0ac99b65
Davide Fissore <fisso9900@gmail.com>
2026-04-18 20:37:05 +0200

    paths

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export"
+  output: "export",
+  // trailingSlash: true,
+  // basePath: "https://fissored.github.io/fatture/"
+  basePath: "/fatture",
 };
 
 export default nextConfig;
